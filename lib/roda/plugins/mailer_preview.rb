abf139986233dd2da4ab695d1e4ce64e823ce184
Adam Daniels <adam@mediadrive.ca>
2017-02-09 23:38:07 -0500

    Extract templates into separate files

--- lib/roda/plugins/mailer_preview.rb
+++ lib/roda/plugins/mailer_preview.rb
@@ -31,77 +31,6 @@ class Roda
     #     preview_index(available_previews)
     #   end
     module MailerPreview
-      TEMPLATE = <<-EOC # :nodoc:
-      <!DOCTYPE html>
-      <html>
-      <head>
-        <meta name="viewport" content="width=device-width">
-        <style type="text/css">
-          body {
-            margin: 0;
-          }
-
-          header {
-            font: 12px "Lucida Grande", sans-serif;
-            border-bottom: 1px solid #ddd;
-            padding: 0.5em;
-            overflow: visible;
-          }
-
-          header dt {
-            width: 80px;
-            float: left;
-            clear: left;
-            text-align: right;
-            color: #7f7f7f;
-            padding: 1px;
-          }
-
-          header dd {
-            margin-left: 90px;
-            padding: 1px;
-          }
-
-          .plain {
-            padding: 1em;
-          }
-        </style>
-      </head>
-      <body>
-        <header>
-          <dl>
-          <% headers.each do |header| %>
-            <dt><%= header.name %>:</dt>
-            <dd><%= header.to_s %></dd>
-          <% end %>
-          </dl>
-        </header>
-
-        <div class="body">
-          <% if mail.content_type =~ /plain/ %>
-            <pre class="plain"><%= mail.body %></div>
-          <% else %>
-            <%= mail.body %>
-          <% end %>
-        </div>
-      </body>
-      </html>
-      EOC
-
-      INDEX_TEMPLATE = <<-EOC # :nodoc:
-      <!DOCTYPE html>
-      <html>
-      <head>
-      </head>
-      <body>
-        <ul>
-          <% urls.each do |url| %>
-          <li><a href="<%= path %><%= url %>"><%= url %></a></li>
-          <% end %>
-        </ul>
-      </body>
-      EOC
-
       module InstanceMethods
         # Pass an instance of +Mail+ to render a preview
         #
@@ -118,7 +47,7 @@ class Roda
             acceptable_headers.include?(field.name)
           end
 
-          render(inline: TEMPLATE, locals: { mail: mail, headers: headers })
+          render(path: __dir__ + "/preview.erb", locals: { mail: mail, headers: headers })
         end
 
         # Pass an array of paths to render an index of available previews
@@ -128,7 +57,7 @@ class Roda
         #     preview_index(available_previews)
         #   end
         def preview_index(urls, path: request.path)
-          render(inline: INDEX_TEMPLATE, locals: { urls: urls, path: path })
+          render(path: __dir__ + "/index.erb", locals: { urls: urls, path: path })
         end
       end
 
